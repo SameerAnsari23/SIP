f7f5d267dd4e6ab92005be91cdd14e4f46a1e60c
SameerAnsari23 <sameeransariscience@gmail.com>
2026-06-11 16:20:01 +0530

    JavaScript

---
+++ JS/1js/script.js
@@ -0,0 +1,47 @@
+console.log(`"Yashwant padhlo.... IAS YAS bano"`)
+
+let Mehul = "Yashwant";
+console.log(Mehul);
+
+Mehul = "Jain";
+console.log(Mehul)
+
+Mehul = 20
+// console.log(Mehul + " Sep");
+console.log(`${Mehul} Sep`)
+
+const Yashwant = "Mehul";
+console.log(Yashwant);
+
+// Yashwant = "Prasad";
+// console.log(Yashwant);
+
+
+// let 1adsd = 30;             // wrong way to declare a variable
+
+
+let a = 10;
+console.log(a)
+
+var b = 30
+console.log(b)
+
+const c = 60;
+console.log(c)
+{
+    let a = 20
+    console.log(a)
+
+    console.log(b)
+    var b = 40
+    console.log(b);
+
+    // console.log(c)
+    const c = 90;
+    console.log(c)
+}
+console.log(a)
+console.log(b)
+// const c = 90;
+console.log(c)
+
